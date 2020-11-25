7bfe65f45592efca26ad127ae9edde963d40a8bd
Vichon Jward <vichonjward@gmail.com>
2020-11-25 04:17:15 -0800

    Immutable tuple to store info

--- functional_programming.py
+++ functional_programming.py
@@ -14,3 +14,27 @@ scientist = collections.namedtuple('scientist',[
     'born',
     'nobel'] ) 
 
+Scientist = (
+    scientist(name="Ada Lovelace", field="math", born=1815, nobel=False),
+    scientist(name="Albert Einstine", field="Physics", born=1879, nobel=True),
+    scientist(name="Emmy Noether", field="math", born=1882, nobel=False),
+    scientist(name="Tu Youyou",field="Chemistyr", born=1930, nobel=True),
+    scientist(name="Ada Youath", field="Chemistry", born=1939, nobel=True),
+    scientist(name="Vera Rubin", field="Astronomy", born=1928, nobel=False),
+    scientist(name="Sally Ride", field="Physics", born=1951, nobel=False),
+    scientist(name="Marie Curie", field="Physics", born=1867, nobel=True),
+    )
+
+
+
+
+for i in Scientist:
+    print(f"{i.name} was a sceintist born in {i.born}, in the field of {i.field}")
+    if i.nobel == 1:
+        print("they won a nobel prize")
+    else:
+        print("they did not win a nobel prize")
+
+
+        
+
