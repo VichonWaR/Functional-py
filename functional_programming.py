2ede9d8e2220091ecb0e80ee611b03dd6dccdf06
Vichon Jward <vichonjward@gmail.com>
2020-11-25 01:29:16 -0800

    developing first steps of functional py

---
+++ functional_programming.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 01:10:59 2020
+
+@author: vichon
+"""
+
+# funtional programming - fundis
+import collections
+
+scientist = collections.namedtuple('scientist',[
+    'name',
+    'field',
+    'born',
+    'nobel'] ) 
+
